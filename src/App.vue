cf5753b88bfd8e71a2859ea688604ae35e3fc4e9
Engill Þór Hölluson <FS484334@fss.is>
2018-11-09 08:50:16 +0000

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>Lokaverkefni</h1>
+      <p>Engill Þór</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
