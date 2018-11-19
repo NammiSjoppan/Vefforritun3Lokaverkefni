84f922202859de2f191a25985445efdc8670074d
Engill Þór Hölluson <FS484334@fss.is>
2018-11-19 11:19:38 +0000

    Added box around everything and added headers

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
-  <div id="app">
-    <div class="container">
-      
+    <div id="app">
+        <div id="masterBox">
+            <div id="headers">
+                <h1 style="border: solid 1px"> Bensín95 </h1>
+                <h1 style="border: solid 1px"> Bensín95 Afsláttur </h1>
+                <h1 style="border: solid 1px"> Diesel </h1>
+                <h1 style="border: solid 1px"> Diesel Afsláttur </h1>
+                <h1 style="border: solid 1px"> Company </h1>
+            </div>
+            <div v-for="result in results" class="box" id="resultContainer">
+                <p> {{ result.bensin95 }} </p>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -12,6 +21,12 @@ import axios from 'axios';
 export default {
 
     name: 'app',
+
+    data() {
+        return{
+            results: []
+        }
+    },
  
     mounted() {
         axios.get("http://apis.is/petrol")
